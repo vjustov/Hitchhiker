67f9737d61fcd75f8b724725827937bcf77fae0d
vjustov <victor.justo@theberrycompany.com>
2013-06-26 08:21:29 -0400

    Initial directory structure and some tests

---
+++ README.md~
@@ -0,0 +1,4 @@
+Hitchhiker
+==========
+
+Hitchhiker's code to the galaxy
---
+++ spec/spec_helper.rb
@@ -0,0 +1,8 @@
+['']
+
+require 'json'
+
+Rspec.configure do |config|
+  config.color_enabled = true
+  config.formatter = :documentation
+end
