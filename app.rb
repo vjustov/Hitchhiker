90704b08f11a380f836c0d3033793dbfbe5a9577
vjustov <victor.justo@theberrycompany.com>
2013-06-28 17:05:20 -0400

    Added some more tests, made some pass

--- app.rb
+++ app.rb
@@ -19,4 +19,39 @@ end
   get '/users' do
     User.all.to_json
   end
+
+  post '/users' do
+    halt 400 if request.params.nil?
+    user = User.new request.params 
+
+    halt 500 unless user.save
+
+    [201, user.to_json]
+  end
+
+  get '/users/drivers' do
+    User.where(hitchhiker: 'false').to_json
+  end
+
+  get '/users/hitchhikers' do
+    User.where(hitchhiker: 'true').to_json
+  end
+
+  put '/users/:id' do
+    
+    user = User.find_by(user_id: params[:id])
+    halt 404 if user.nil?
+
+    halt 400 if params.to_json.nil?
+
+    %w(username hitchhiker).each do |key|
+      unless params[key].nil? || params[key] == user[key]
+        user[key] = params[key]
+      end
+    end
+    
+    halt 500 unless user.save
+      
+    end
+  # end
 # end
--- lib/user.rb
+++ lib/user.rb
@@ -2,6 +2,10 @@ require 'mongoid'
 
 class User
   include Mongoid::Document
+  # field :user_id, type: Integer
   field :username, type: String
   field :hitchhiker, type: Boolean
+
+  # validates_uniqueness_of :user_id
+  # validates_numericality_of :user_id, only_integer: true
 end
