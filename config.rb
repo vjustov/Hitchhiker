3ca592ff896fbc9efad8cad11065b02c727713ca
Dariel Javier <dariel.javier@gmail.com>
2013-07-14 10:42:36 -0400

    add production env

--- config.rb
+++ config.rb
@@ -10,29 +10,45 @@ require "rack/oauth2/server/admin"
 class Hitchhicker_config < Sinatra::Base
   register Rack::OAuth2::Sinatra
 
-configure :development do
-  oauth.database = Mongo::Connection.new["API_TEST"]
-   oauth.param_authentication = true
-  
-  Bundler.setup(:default, :assets, :development)
-  set :environment, :development
-  enable :sessions, :logging, :static, :inline_templates, :method_override, :dump_errors, :run
-  Mongoid.load!(File.join(File.dirname(__FILE__),'mongoid.yaml'), :development)
-end
+  configure :development do
+    oauth.database = Mongo::Connection.new["API_TEST"]
+     oauth.param_authentication = true
     
-configure :test do
-  
-  oauth.database = Mongo::Connection.new["API_TEST"]
-  oauth.param_authentication = true
-  
-  #oauth.authenticator = lambda do |id, client_secret|
-  #  user = Rack::OAuth2::Server::Client.find(id)
-  #  user.id if user.client_secret =client_secret
-  #end
-  
-  Bundler.setup(:default, :assets, :test)
-  set :environment, :test
-  enable :sessions, :logging, :static, :inline_templates, :method_override, :dump_errors, :run
-  Mongoid.load!(File.join(File.dirname(__FILE__),'mongoid.yaml'), :test)
-end
+    Bundler.setup(:default, :assets, :development)
+    set :environment, :development
+    enable :sessions, :logging, :static, :inline_templates, :method_override, :dump_errors, :run
+    Mongoid.load!(File.join(File.dirname(__FILE__),'mongoid.yaml'), :development)
+  end
+      
+  configure :test do
+    
+    oauth.database = Mongo::Connection.new["API_TEST"]
+    oauth.param_authentication = true
+    
+    #oauth.authenticator = lambda do |id, client_secret|
+    #  user = Rack::OAuth2::Server::Client.find(id)
+    #  user.id if user.client_secret =client_secret
+    #end
+    
+    Bundler.setup(:default, :assets, :test)
+    set :environment, :test
+    enable :sessions, :logging, :static, :inline_templates, :method_override, :dump_errors, :run
+    Mongoid.load!(File.join(File.dirname(__FILE__),'mongoid.yaml'), :test)
+  end
+
+  configure :production do
+    
+    oauth.database = Mongo::Connection.new["API_PROD"]
+    oauth.param_authentication = true
+    
+    #oauth.authenticator = lambda do |id, client_secret|
+    #  user = Rack::OAuth2::Server::Client.find(id)
+    #  user.id if user.client_secret =client_secret
+    #end
+    
+    Bundler.setup(:default, :assets, :test)
+    set :environment, :production
+    enable :sessions, :logging, :static, :inline_templates, :method_override, :dump_errors, :run
+    Mongoid.load!(File.join(File.dirname(__FILE__),'mongoid.yaml'), :production)
+  end
 end
